61bd4a4121e7858650143481d76001c36874ebef
arwn <windham.aren@protonmail.com>
2019-09-26 15:28:31 -0700

    done nowe

--- src/main/java/swingy/Main.java
+++ src/main/java/swingy/Main.java
@@ -5,24 +5,23 @@ import swingy.view.SwingView;
 
 public class Main
 {
+    private static String usage = "Usage: swingy [gui, console]";
     private static void die(String msg)
     {
         System.err.println(msg);
         System.exit(1);
     }
 
-    public static void main(String[] args)
+    public static void main(String args[])
     {
-        if (args.length != 1) {
-            new Game(new SwingView());
-        } else {
-            if (args[0].equals("gui")) {
+        if (args.length == 1) {
+            if (args[0].equals("gui"))
                 new Game(new SwingView());
-            } else if (args[0].equals("cli")) {
+            else if (args[0].equals("console"))
                 new Game(new ConsoleView());
-            } else {
-                die("Invalid args");
-            }
-        }
+            else
+                System.out.println(usage);
+        } else
+            System.out.println(usage);
     }
 }
